f55be840524577c12d7ad56f347f74a5cc3aacab
LuLubik <pigas635@mail.ru>
2025-05-26 16:30:24 +0300

    Добавлен .gitignore, удалены из индекса временные и сгенерированные файлы

--- IdeaProjects/WalletService/target/classes/db/script/create_wallets_table.sql
+++
@@ -1,7 +0,0 @@
---change author:Lubov-Kolesnikova
-CREATE TABLE IF NOT EXISTS wallets (
-    wallet_id UUID PRIMARY KEY,
-    balance NUMERIC(19, 2) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-    );
